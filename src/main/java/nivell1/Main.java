085c93dc0c859fca403c62ff903202e5867847e3
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-04-02 17:17:48 +0200

    enunciats

---
+++ src/main/java/nivell1/Main.java
@@ -0,0 +1,20 @@
+package nivell1;
+
+/*
+En tots els exercicis, crea un projecte Gradle amb classe Main que demostri l'ús del patró
+ (amb les invocacions que necessiti).
+
+Observer
+Dissenya un sistema en el qual un Agent de Borsa (Observable) notifica a diverses agències de Borsa (Observers)
+ canvis quan la Borsa puja o baixa.
+
+Serà necessari que l'objecte Observable mantingui referències als Observers.
+Encara que ha estat deprecada a partir de Java 9, pot utilitzar la interfície java.util.Observer si ho desitges
+ */
+
+
+public class Main {
+
+
+
+}
---
+++ src/main/java/nivell1/Observer.java
@@ -0,0 +1,4 @@
+package nivell1;
+
+public class Observer {
+}
